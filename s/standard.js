d33fd236d17b8d5ec3dbed69fb09d7c162430366
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-03-21 23:12:29 +1300

    Try removing host from js.

--- s/standard.js
+++ s/standard.js
@@ -3,7 +3,7 @@ var loginshowing = false;
 
 function initialise_charsheet()
 {
-  $.getJSON('http://nexus.gen.nz:8080/csjson',
+  $.getJSON('/csjson',
     function(data){
       charsheet = JSON.parse(JSON.stringify(data));
       charsheetadjusted = JSON.parse(JSON.stringify(data));
@@ -133,7 +133,7 @@ function actionise()
 function get_action_queue_from_server()
 {
   $(".messagepane").html("");
-  $.getJSON('http://nexus.gen.nz:8080/showactions',
+  $.getJSON('/showactions',
     function(data){
       $.each(data['pastactions'], function(i,item){
         if (item.type)
@@ -148,7 +148,7 @@ function get_action_queue_from_server()
 
 function post_action_queue_to_server(aq)
 {
-  $.getJSON('http://nexus.gen.nz:8080/submitactions', aq,
+  $.getJSON('/submitactions', aq,
     function(data){
       $('.messagepane').html(data['message']);
       $.each(data['pastactions'], function(i,item){
